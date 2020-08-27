748dd8f80546f57b9a9a598c8d72ea448b18a47f
deboralumempouw <s11810214@student.unklab.ac.id>
2020-08-27 16:37:36 +0700

    menambahkan pertemuan2,pertemuan3,pertemuan4

---
+++ pertemuan3/index.js
@@ -0,0 +1,37 @@
+/*
+    DASAR-DASAR PEMOGRAMAN JAVASCRIPTH
+    1.
+    2.operator
+    3.array
+    4.object
+    5.fungsi
+    6.JavaScripth Conditional
+    7.JavaScripth Loog
+
+/*
+//===============================
+/*
+    1.Varibel dan tipe data
+*/
+
+const firstName ="John";
+let age = 20;
+age = 20;
+const isMarried = true;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(age + " "+ isMarried);
+
+const info =
+"My name is" + firstName +"I'm" + age + "years old. " + isMarried;
+
+console.log (typeof info);
+
+function helloworld() {
+    var hello = "helloWorld";
+    console.log(hello);
+}
+helloworld();
+
