5f0b9bd8b14c1795c06bdef6472c6f0110c88a12
deboralumempouw <s11810214@student.unklab.ac.id>
2020-11-03 15:17:05 +0700

    TEST#2

---
+++ Test02/index.js
@@ -0,0 +1,62 @@
+//1.1
+/*
+const calAge = (birthYear, currentYear) 
+{
+    return 2019 - birthYear;
+
+}
+
+const yearUntilRetirement= (year, firstName) 
+{
+    const age = 30;
+    const retirement = 60 - age;
+
+    if(retirement > 0) 
+    {
+        console.log(`${firstName} retired in ${retirement} years`);
+    }
+    else
+    {
+        console.log(`${firstName} is already retired.`);
+    }
+}
+yearUntilRetirement(1987, {"John"});
+*/
+
+//1.2
+/*
+const addNumber =(a,b,c,d,e,f,g)
+{
+    const numbers =[a,b,c,d,e,f,];
+    let total =0;
+    for(let i=0; i < numbers.legth; i++)
+    {
+        total = total + numbers[i];
+    }
+    return total;
+}
+
+    console.log(addNumber(1,2,3,4,5,6,7));
+*/
+//2.1
+/*
+const warnaKesukaan = (warna) =>
+{
+    return console.log(`Warna Kesukaan saya adalah ${warna}`);
+
+}
+warnaKesukaan("Putih");
+*/
+
+//2.2
+// Spread Operator
+/*
+let namaBuah1 = ['apel','mangga'];
+let namaBuah2 = ['sirsak','semangka'];
+
+let semuaNamaBuah;
+
+namaBuah1 = [...namaBuah1,...namaBuah2];
+
+console.log(namaBuah1);
+*/
