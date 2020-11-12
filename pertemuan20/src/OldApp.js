703313e3aec1878c88f33695faabd620e9689fb1
deboralumempouw <s11810214@student.unklab.ac.id>
2020-11-13 01:39:58 +0700

    MenggabungkanPertemuan21dan22

---
+++ pertemuan20/src/OldApp.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Hello from './components/Hello';
+import Contents from './components/Contents';
+
+function App() {
+  return (
+    <>
+     <Header/>
+      <Hello/>
+      <Contents/>
+    </>
+  );
+}
+
+export default App;
---
+++ pertemuan20/src/Utils/person.js
@@ -0,0 +1,32 @@
+export const person = [
+    {
+        name: "Vindy Muasa",
+        institution: "Universitas Klabat",
+        address: "Airmadidi - Minahasa Utara",
+        phoneNumber: "08124258xxxx",
+    },
+    {
+        name: "Angelina Doringin",
+        institution: "Universitas Klabat",
+        address: "Airmadidi - Minahasa Utara",
+        phoneNumber: "08953374xxxx",
+    },
+    {
+        name: "Debora Lumempouw",
+        institution: "Universitas Klabat",
+        address: "Airmadidi - Minahasa Utara",
+        phoneNumber: "08534378xxxx",
+    },
+    {
+        name: "Chelsea Wowor",
+        institution: "Universitas Klabat",
+        address: "Airmadidi - Minahasa Utara",
+        phoneNumber: "08524279xxxx",
+    },
+    {
+        name: "Eirene Wowor",
+        institution: "Universitas Klabat",
+        address: "Airmadidi - Minahasa Utara",
+        phoneNumber: "08574873xxxx",
+    }
+];
